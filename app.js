73c523f51a02ff68b2bbc1a5a5b3f0cc3c390d2c
Jonas esteves <Jonasesteves2006@gmail.com>
2025-05-24 00:48:58 -0300

    Adicionando Array dos objetos

---
+++ app.js
@@ -0,0 +1,12 @@
+const produtos = [
+  { nome: "Playstation 5", preco: 4000, categoria: "console", disponibilidade: true },
+  { nome: "Xbox Series X", preco: 4100, categoria: "console", disponibilidade: false },
+  { nome: "GTA VI", preco: 9999, categoria: "jogos", disponibilidade: false },
+  { nome: "Controle sem fio DualSense", preco: 400, categoria: "acessorios", disponibilidade: true },
+  { nome: "FIFA 2029", preco: 9.99, categoria: "jogos", disponibilidade: true },
+  { nome: "Nintendo Switch 2", preco: 4500, categoria: "console", disponibilidade: false },
+  { nome: "Headset sem fio PULSE Elite", preco: 800, categoria: "acessorios", disponibilidade: false },
+  { nome: "Donkey Kong Country™ Returns HD", preco: 349, categoria: "jogos", disponibilidade: false },
+  { nome: "Controle sem fio Xbox", preco: 500, categoria: "acessorios", disponibilidade: true },
+  { nome: "Red Dead Redemptiom 2", preco: 350, categoria: "jogos", disponibilidade: true },
+];
