4cf716372daac5a92b4b028c5544000f320da443
Augusto Valerio <augusto.valerio06@gmail.com>
2025-05-24 00:25:28 -0300

    Inicio JavaScript

---
+++ app.js
@@ -0,0 +1,7 @@
+const produtos = [
+  { nome: "Playstation 5", preco: 4000, categoria: "console", disponibilidade: true },
+  { nome: "Xbox Series X", preco: 4100, categoria: "console", disponibilidade: false },
+  { nome: "GTA VI", preco: 9999, categoria: "jogos", disponibilidade: false },
+  { nome: "Controle sem fio DualSense", preco: 400, categoria: "acessorios", disponibilidade: true },
+  { nome: "FIFA 2029", preco: 9.99, categoria: "jogos", disponibilidade: true },
+];
