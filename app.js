076c4c36ad51609e9f7380d052c8b124d6eeb0b4
Augusto Valerio <augusto.valerio06@gmail.com>
2025-05-24 01:25:53 -0300

    Função de Filtrar e evento de click adicionado

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const produtos = [
-  { nome: "Playstation 5", preco: 4000, categoria: "console", disponibilidade: true },
-  { nome: "Xbox Series X", preco: 4100, categoria: "console", disponibilidade: false },
+  { nome: "Playstation 5", preco: 4000, categoria: "consoles", disponibilidade: true },
+  { nome: "Xbox Series X", preco: 4100, categoria: "consoles", disponibilidade: false },
   { nome: "GTA VI", preco: 9999, categoria: "jogos", disponibilidade: false },
   { nome: "Controle sem fio DualSense", preco: 400, categoria: "acessorios", disponibilidade: true },
   { nome: "FIFA 2029", preco: 9.99, categoria: "jogos", disponibilidade: true },
-  { nome: "Nintendo Switch 2", preco: 4500, categoria: "console", disponibilidade: false },
+  { nome: "Nintendo Switch 2", preco: 4500, categoria: "consoles", disponibilidade: false },
   { nome: "Headset sem fio PULSE Elite", preco: 800, categoria: "acessorios", disponibilidade: false },
   { nome: "Donkey Kong Country™ Returns HD", preco: 349, categoria: "jogos", disponibilidade: false },
   { nome: "Controle sem fio Xbox", preco: 500, categoria: "acessorios", disponibilidade: true },
@@ -12,21 +12,35 @@ const produtos = [
 ];
 
 function listarProdutos(lista) {
-    const container = document.getElementById("container");
-    container.innerHTML = "";
-  
-    lista.forEach((produto) => {
-      const div = document.createElement("div");
-      div.classList.add("produto");
-      div.innerHTML = `
-        <h3>${produto.nome}</h3>
-        <p>Preço: R$ ${produto.preco.toFixed(2)}</p>
-        <p>Categoria: ${produto.categoria}</p>
-        <p>${produto.disponibilidade ? "Disponível" : "Indisponível"}</p>`;
-      container.append(div);
+  const container = document.getElementById("container");
+  container.innerHTML = "";
 
-      });
-    };
+  lista.forEach((produto) => {
+    const div = document.createElement("div");
+    div.classList.add("produto");
+    div.innerHTML = `
+      <h3>${produto.nome}</h3>
+      <p>Preço: R$ ${produto.preco.toFixed(2)}</p>
+      <p>Categoria: ${produto.categoria}</p>
+      <p>${produto.disponibilidade ? "Disponível" : "Indisponível"}</p>`;
+    container.append(div);
+
+    });
+  };
 
 const listarTodos = document.querySelector("#listarTodos");
 listarTodos.addEventListener("click", () => listarProdutos(produtos));
+
+const botao = document.querySelector("#filtrar");
+botao.addEventListener("click", () => {
+  const categoria = document.getElementById("categoria").value;
+  const somenteDisponiveis = document.getElementById("disponibilidade").checked;
+
+  const produtosFiltrados = produtos.filter((produto) => {
+  const verificarCategoria = categoria === "todas" || produto.categoria === categoria;
+  const verificarDisponibilidade = !somenteDisponiveis || produto.disponibilidade === somenteDisponiveis;
+  return verificarCategoria && verificarDisponibilidade;
+  });
+
+  listarProdutos(produtosFiltrados);
+});
