24579d653c161310a9d7c7ad7784fab66e8f33f1
Augusto Valerio <augusto.valerio06@gmail.com>
2025-05-24 23:07:44 -0300

    Organização dos nomes dos objetos das listas

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const produtos = [
   { nome: "Playstation 5", preco: 4000, categoria: "consoles", disponibilidade: true, imagem: "./images/ps5.png"},
   { nome: "Xbox Series X", preco: 4100, categoria: "consoles", disponibilidade: false, imagem: "./images/xboxSeriesX.png"},
-  { nome: "GTA VI", preco: 9999, categoria: "jogos", disponibilidade: false, imagem: "./images/gtaVI.png"},
-  { nome: "Controle sem fio DualSense", preco: 400, categoria: "acessorios", disponibilidade: true, imagem: "./images/dualsense.png"},
-  { nome: "FIFA 2029", preco: 9.99, categoria: "jogos", disponibilidade: true, imagem: "./images/fifa29.png"},
   { nome: "Nintendo Switch 2", preco: 4500, categoria: "consoles", disponibilidade: false, imagem: "./images/switch2.png"},
-  { nome: "Headset sem fio PULSE Elite", preco: 800, categoria: "acessorios", disponibilidade: false, imagem: "./images/pulseElite.png"},
+  { nome: "GTA VI", preco: 9999, categoria: "jogos", disponibilidade: false, imagem: "./images/gtaVI.png"},
+  { nome: "FIFA 2029", preco: 9.99, categoria: "jogos", disponibilidade: true, imagem: "./images/fifa29.png"},  
   { nome: "Donkey Kong Country™ Returns HD", preco: 349, categoria: "jogos", disponibilidade: false, imagem: "./images/donkeyKong.png"},
-  { nome: "Controle sem fio Xbox", preco: 500, categoria: "acessorios", disponibilidade: true, imagem: "./images/controleSeriesX.png"},
   { nome: "Red Dead Redemption 2", preco: 350, categoria: "jogos", disponibilidade: true, imagem: "./images/redDead.png"},
+  { nome: "Controle sem fio Xbox", preco: 500, categoria: "acessorios", disponibilidade: true, imagem: "./images/controleSeriesX.png"},
+  { nome: "Controle sem fio DualSense", preco: 400, categoria: "acessorios", disponibilidade: true, imagem: "./images/dualsense.png"},
+  { nome: "Headset sem fio PULSE Elite", preco: 800, categoria: "acessorios", disponibilidade: false, imagem: "./images/pulseElite.png"},
 ];
 
 function listarProdutos(lista) {
@@ -16,7 +16,7 @@ function listarProdutos(lista) {
   container.innerHTML = "";
   container.classList.add("containerProdutos");
   container.style.display = "grid";
-  container.style.gridTemplateColumns = "repeat(2, 1fr)";
+  container.style.gridTemplateColumns = "repeat(3, 1fr)";
   container.style.margin = "auto";
   container.style.textAlign = "center";
   container.style.width = "50%";
